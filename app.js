fc5dceeadd251adee71b66a85e3060170f1f27b8
Craybnirob <74036538+Craybnirob@users.noreply.github.com>
2021-01-20 18:45:22 +0600

    SideNav

---
+++ app.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById('mySidenav').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+    document.getElementById('demo').style.marginLeft = '250px';
+}
+// close Nav
+function closeNav() {
+    document.getElementById('mySidenav').style.width = '0px';
+    document.getElementById('main').style.marginLeft = '0px';
+    document.getElementById('demo').style.marginLeft = '0px';
+}
